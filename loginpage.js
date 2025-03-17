83fb2f890019fa581539b9d03af1e2a9acba1b2b
Anthony <domasiganthony139@gmail.com>
2025-03-17 22:44:43 +0800

    js code fixed

--- loginpage.js
+++ loginpage.js
@@ -1,3 +1,10 @@
+var user_input = document.getElementById('login_username');
+var pass_input = document.getElementById('login_password');
+
+const user = 'admin123';
+const password = 'root1234';
+
+
 document.querySelector('#show_register').addEventListener('click', ()=> {
   document.getElementById('formwrapper').style.transform = 'translateX(-100%)';
 });
@@ -6,33 +13,24 @@ document.querySelector('#show_login').addEventListener('click', ()=> {
   document.getElementById('formwrapper').style.transform = 'translateX(0)';
 });
 
-var user_input = document.getElementById('login_username');
-var pass_input = document.getElementById('login_password');
-
-var user = 'admin123';
-var password = 'root1234';
 
 function validate() {
-  if (user_input.value.length == 0 || pass_input.value.length == 0) {
-    alert('Input fields cannot be empty.')
-    return false;
-  } 
-  else if (user_input.value != user || pass_input.value != password) 
-  {
-    alert('Incorrect Username or Password');
-    return false;
-  } 
-  else {
+  if(user_input.value === user && pass_input.value === password) {
     window.location.href = 'index.html';
-  }
+  } else if ((user_input.value).length == 0 || (pass_input.value).length == 0) {
+    alert('Input fields cannot be empty!');
+  } else {
+    alert('Invalid Input!');
+  };
 };
 
-function validate_info() {
-  var new_user = document.getElementById('new_username').value;
-  var new_pass = document.getElementById('new_password').value;
-  var confirm_pass = document.getElementById('confirm_password').value;
 
-  if (confirm_pass != new_pass) {
+function validate_info() {
+  var new_user = document.getElementById('new_username');
+  var new_pass = document.getElementById('new_password');
+  var confirm_pass = document.getElementById('confirm_password');
+
+  if (confirm_pass.value != new_pass.value) {
     alert('Password does not match. Please try again.');
     return false;
   };
